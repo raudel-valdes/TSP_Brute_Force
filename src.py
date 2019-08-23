d5063ceaf6b3e06e17a5d16119701f03265980a9
Raudel Valdes <raudel.valdes2015@gmail.com>
2019-08-23 10:33:18 -0400

    first commit. Added functionality for reading the .tsp files

---
+++ src.py
@@ -0,0 +1,27 @@
+import sys
+import os
+from re import sub
+
+"""
+Loops through each json files provided on the command line and passes each file
+to the parser
+"""
+def main(argv):
+  if len(argv) < 2:
+      print('Usage: python3 Random#.tsp <path to tsp file>', file=sys.stderr)
+      sys.exit(1)
+
+  # loops over all tsp files in the argument
+  for f in argv[1:]:
+      if f.endswith('.tsp'):
+        cordinates = open(f,'r')
+        print("Success parsing " + f)
+        print('**********************************************************')
+        print(cordinates.read())
+        cordinates.close()
+        print('**********************************************************')
+        
+      else print("This script only works with extension .tsp")
+            
+if __name__ == '__main__':
+    main(sys.argv)
